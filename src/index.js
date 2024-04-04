ecd2ce2d4094d0c6a69c9760e9813ad9f118aa6b
Collins Tatang <makungongcollins@gmail.com>
2024-04-05 00:07:12 +0100

    Add tailwind css for styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
