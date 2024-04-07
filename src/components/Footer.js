fedb6c156fe3d351015a3c4ba599a80faae75197
Collins Tatang <makungongcollins@gmail.com>
2024-04-07 23:41:03 +0100

    Add footers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,41 @@
+import {
+  Xicon,
+  LinkedinIcon,
+} from './Icons';
+
+const navigation = [
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/CollinsTatang1',
+    icon: (props) => <Xicon {...props} />,
+  },
+  {
+    name: 'LinkedinIcon',
+    href: 'https://www.linkedin.com/in/makungong-collins/',
+    icon: (props) => <LinkedinIcon {...props} />,
+  },
+];
+
 function Footer() {
   return (
-    <div>
-      <h1>Welcome to Footer Page</h1>
-    </div>
+    <footer className="bg-gray-700">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-xs leading-5 text-gray-500">
+            &copy; 2024 Collins Tatang, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
+
 export default Footer;
---
+++ src/components/Icons.js
@@ -0,0 +1,46 @@
+export const Xicon = (props) => (
+  <svg
+    width="25"
+    height="22"
+    viewBox="0 0 512 512"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+      fill="white"
+    />
+  </svg>
+);
+
+export const Facebook = (props) => {
+  <svg
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+      clipRule="evenodd"
+    />
+  </svg>;
+};
+
+export const LinkedinIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    width="100%"
+    height="100%"
+    {...props}
+  >
+    <path
+      d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
+      fill="white"
+    />
+  </svg>
+);
