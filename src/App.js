db42a088d9dbec45444baa5cb4af3c3fa214cbb0
Collins Tatang <makungongcollins@gmail.com>
2024-04-16 05:57:06 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
     <div className="">
       <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
